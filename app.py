c03f5e23f9b92a0965c000c0c3681b39b890c594
Hilmi Can Taşkıran <hlmtaskiran76@gmail.com>
2021-07-06 11:40:09 +0300

    update/app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ api = Api(app, errors=errors)
 bcrypt = Bcrypt(app)
 jwt = JWTManager(app)
 
-app.config['MONGODB_HOST'] = 'mongodb+srv://hilmican:dpmd1930@intern.51qqm.mongodb.net/intern?retryWrites=true&w=majority'
+app.config['MONGODB_HOST'] = 'mongodb+srv://hilmican:dpmd1930@intern.51qqm.mongodb.net/influanceinsta?retryWrites=true&w=majority'
 
 initialize_db(app)
 initialize_routes(api)
