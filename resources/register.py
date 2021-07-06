b25c08a83c81ddc69108f2f6e77ba5c6b06abafb
hilmicantaskiran <hlmtaskiran76@gmail.com>
2021-07-06 10:22:01 +0300

    fix/register

--- resources/register.py
+++ resources/register.py
@@ -2,25 +2,46 @@ from flask import Response, request
 from flask_jwt_extended import jwt_required
 from database.models import Register
 from flask_restful import Resource
-from mongoengine.errors import FieldDoesNotExist, NotUniqueError, ValidationError
-from resources.errors import SchemaValidationError, UserNameAlreadyExistsError, InternalServerError
+from mongoengine.errors import DoesNotExist, InvalidQueryError
+from resources.errors import SchemaValidationError, InternalServerError, UpdatingUserNameError, DeletingUserNameError, \
+    UserNameNotExistsError
 
 
-class RegisterApi(Resource):
+class RegisterGetApi(Resource):
     def get(self):
         user_name = Register.objects().to_json()
         return Response(user_name, mimetype="application/json", status=200)
 
+
+class RegisterApi(Resource):
     @jwt_required()
-    def post(self):
+    def put(self, email):
         try:
             body = request.get_json(force=True)
-            user_name = Register(**body).save()
-            id = user_name.id
-            return {'id': str(id)}, 200
-        except (FieldDoesNotExist, ValidationError):
+            Register.objects.get(email=email).update(**body)
+            return '', 200
+        except InvalidQueryError:
             raise SchemaValidationError
-        except NotUniqueError:
-            raise UserNameAlreadyExistsError
+        except DoesNotExist:
+            raise UpdatingUserNameError
+        except Exception:
+            raise InternalServerError
+
+    @jwt_required()
+    def delete(self, email):
+        try:
+            Register.objects.get(email=email).delete()
+            return '', 200
+        except DoesNotExist:
+            raise DeletingUserNameError
+        except Exception:
+            raise InternalServerError
+
+    def get(self, email):
+        try:
+            user_name = Register.objects.get(email=email).to_json()
+            return Response(user_name, mimetype="application/json", status=200)
+        except DoesNotExist:
+            raise UserNameNotExistsError
         except Exception:
             raise InternalServerError
--- resources/routes.py
+++ resources/routes.py
@@ -1,5 +1,5 @@
 from resources.gender import GendersAgesApi, GenderAgeApi
-from resources.register import RegisterApi
+from resources.register import RegisterGetApi, RegisterApi
 from resources.auth import SignupApi, LoginApi
 
 
@@ -7,8 +7,8 @@ def initialize_routes(api):
     api.add_resource(GendersAgesApi, '/api/gender')
     api.add_resource(GenderAgeApi, '/api/gender/<username>')
 
-    api.add_resource(RegisterApi, '/api/register')
+    api.add_resource(RegisterGetApi, '/api/register')
+    api.add_resource(RegisterApi, '/api/register/<email>')
 
     api.add_resource(SignupApi, '/api/auth/signup')
     api.add_resource(LoginApi, '/api/auth/login')
-
